d8119c17bb7433f96ee027971cfc2452d444fafc
iLexN <iLexN@users.noreply.github.com>
2021-07-09 11:29:16 +0800

    Update rector-ci.php

--- rector-ci.php
+++ rector-ci.php
@@ -9,25 +9,19 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set(Option::SETS, [
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::CODING_STYLE,
-        SetList::PHP_71,
-        SetList::PHP_72,
-        SetList::PHP_73,
-        SetList::PHP_74,
-        SetList::PHP_80,
-        SetList::PSR_4,
-        SetList::CODE_QUALITY_STRICT,
-        SetList::EARLY_RETURN,
-        SetList::NAMING,
-        SetList::PHPUNIT_70,
-        SetList::PHPUNIT_80,
-        SetList::PHPUNIT_90,
-        SetList::PHPUNIT_91,
-    ]);
+    $containerConfigurator->import(SetList::CODE_QUALITY);
+    $containerConfigurator->import(SetList::DEAD_CODE);
+    $containerConfigurator->import(SetList::CODING_STYLE);
+    $containerConfigurator->import(SetList::PHP_80);
+    $containerConfigurator->import(SetList::PSR_4);
+    $containerConfigurator->import(SetList::CODE_QUALITY_STRICT);
+    $containerConfigurator->import(SetList::EARLY_RETURN);
+    $containerConfigurator->import(SetList::NAMING);
 
-    $parameters->set(Option::SKIP, []);
+    $parameters->set(Option::SKIP, [
+        Rector\Php80\Rector\FunctionLike\UnionTypesRector::class,
+        Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector::class,
+        Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector::class,
+    ]);
 
 };
