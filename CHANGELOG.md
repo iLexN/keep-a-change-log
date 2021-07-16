9c8c920a162622616e02a7bde30a6aff8eefb200
iLexN <ilex.job@gmail.com>
2021-07-16 17:21:32 +0800

    Add composer bin command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.3.0] - 2021-07-14
+### Added
+- Add bin command
+
 ## [1.2.0] - 2021-03-04
 ### Added
 - Add github CI
@@ -50,6 +54,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ### Added
 - First release
 
+[1.3.0]: https://github.com/iLexN/keep-a-change-log/compare/1.2.0...1.3.0
 [1.2.0]: https://github.com/iLexN/keep-a-change-log/compare/1.1.0...1.2.0
 [1.1.0]: https://github.com/iLexN/keep-a-change-log/compare/1.0.0...1.1.0
 [1.0.0]: https://github.com/iLexN/keep-a-change-log/compare/0.0.3...1.0.0
---
+++ bin/ilexn-kcg.php
@@ -0,0 +1,71 @@
+#!/usr/bin/env php
+<?php
+declare(strict_types=1);
+
+use Ilex\ChangeLog\ChangeLog;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\SingleCommandApplication;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+$possibleAutoloadPaths = [
+    // local dev repository
+    __DIR__ . '/../vendor/autoload.php',
+    // dependency
+    __DIR__ . '/../../../../vendor/autoload.php',
+];
+
+$isAutoloadFound = false;
+foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
+    if (file_exists($possibleAutoloadPath)) {
+        require_once $possibleAutoloadPath;
+        +$isAutoloadFound = true;
+        break;
+    }
+}
+
+if ($isAutoloadFound === false) {
+    throw new RuntimeException(sprintf(
+        'Unable to find "vendor/autoload.php" in "%s" paths.',
+        implode('", "', $possibleAutoloadPaths)
+    ));
+}
+
+(new SingleCommandApplication())
+    ->setName('Keep a change log') // Optional
+    ->addArgument('path', InputArgument::REQUIRED, 'Config path')
+    ->addArgument('destination', InputArgument::OPTIONAL,
+        'CHANGELOG.md destination', '.')
+    ->setCode(static function (
+        InputInterface $input,
+        OutputInterface $output
+    ): int {
+        $io = new SymfonyStyle($input, $output);
+
+        /** @var string $path */
+        $path = $input->getArgument('path');
+        $target = $input->getArgument('destination');
+        $file = $target . '/CHANGELOG.md';
+
+        $io->section('Reading config file');
+        $io->info('Config file path: ' . $path);
+        if (!file_exists($path)) {
+            $io->error('file not exist');
+            return Command::FAILURE;
+        }
+        $io->info('Changelog file path: ' . $file);
+
+        /** @var ChangeLog $config */
+        $changeLog = include $path;
+        $result = \file_put_contents($file, $changeLog->render());
+        if ($result === false) {
+            $io->error('Cannot write file');
+        } else {
+            $io->info('Success');
+        }
+
+        return Command::SUCCESS;
+    })
+    ->run();
