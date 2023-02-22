5b2571451d26878aa48ffccfede8375f75d32c41
iLexN <iLexN@users.noreply.github.com>
2023-02-22 16:11:22 +0800

    Update rector-ci.php

--- rector-ci.php
+++ rector-ci.php
@@ -15,7 +15,6 @@ return static function (RectorConfig $rectorConfig): void {
         SetList::PSR_4,
         SetList::EARLY_RETURN,
         SetList::NAMING,
-        SetList::TYPE_DECLARATION_STRICT,
     ]);
 
 
